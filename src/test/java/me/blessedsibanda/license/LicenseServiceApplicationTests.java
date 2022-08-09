cd0c07813d39f987c93368b10d2d4d1fe3b1c03b
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-09 05:05:59 +0200

    init project

---
+++ src/test/java/me/blessedsibanda/license/LicenseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.blessedsibanda.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
