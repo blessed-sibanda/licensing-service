5d8131eaf6c67fe6f638676f06450dbbc6b295d6
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-09 06:11:35 +0200

    add internationalization

--- src/main/java/me/blessedsibanda/license/controller/LicenseController.java
+++ src/main/java/me/blessedsibanda/license/controller/LicenseController.java
@@ -1,5 +1,7 @@
 package me.blessedsibanda.license.controller;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,9 +42,10 @@ public class LicenseController {
     @PostMapping
     public ResponseEntity<String> createLicense(
             @PathVariable("organizationId") String organizationId,
-            @RequestBody License license) {
+            @RequestBody License license,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
         return ResponseEntity.ok(licenseService.createLicense(
-                license, organizationId));
+                license, organizationId, locale));
     }
 
     @DeleteMapping("/{licenseId}")
--- src/main/java/me/blessedsibanda/license/service/LicenseService.java
+++ src/main/java/me/blessedsibanda/license/service/LicenseService.java
@@ -1,13 +1,19 @@
 package me.blessedsibanda.license.service;
 
+import java.util.Locale;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 
 import me.blessedsibanda.license.model.License;
 
 @Service
 public class LicenseService {
+    @Autowired
+    MessageSource messages;
+
     public License getLicense(String licenseId, String organizationId) {
         License license = new License();
         license.setId(new Random().nextInt(1000));
@@ -20,11 +26,13 @@ public class LicenseService {
         return license;
     }
 
-    public String createLicense(License license, String organizationId) {
+    public String createLicense(License license, String organizationId, Locale locale) {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the post and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage(
+                    "license.create.message", null, locale),
+                    license.toString());
         }
         return responseMessage;
     }
@@ -33,14 +41,16 @@ public class LicenseService {
         String responseMessage = null;
         if (license != null) {
             license.setOrganizationId(organizationId);
-            responseMessage = String.format("This is the put and the object is: %s", license.toString());
+            responseMessage = String.format(messages.getMessage(
+                    "license.update.message", null, null),
+                    license.toString());
         }
         return responseMessage;
     }
 
     public String deleteLicense(String licenseId, String organizationId) {
-        var responseMessage = String.format(
-                "Deleting license with id %s for organization %s",
+        var responseMessage = String.format(messages.getMessage(
+                "license.delete.message", null, null),
                 licenseId,
                 organizationId);
         return responseMessage;
